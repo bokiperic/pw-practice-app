73a2cad8c82c3f194d21f60a0bfbf920efbda9e3
Bojan Peric <bperic.dev@gmail.com>
2024-07-20 17:41:42 +0200

    update first test with hooks and test suites

--- tests/firstTest.spec.ts
+++ tests/firstTest.spec.ts
@@ -1,7 +1,37 @@
 import {test} from '@playwright/test'
 
-test('The first test', async ({page}) => {
+test.beforeEach(async({page}) => {
     await page.goto('http://localhost:4200/')
-    await page.getByText('Forms').click()
-    await page.getByText('Form Layouts').click()
 })
+
+test.describe('Test Suite 1', () => {
+    
+    test.beforeEach(async({page}) => {
+        await page.getByText('Forms').click()
+    })
+
+    test('Navigate to Form Layouts page', async ({page}) => {
+        await page.getByText('Form Layouts').click()
+    })
+    
+    test('Navigate to Datepicker page', async ({page}) => {
+        await page.getByText('Datepicker').click()
+    })
+})
+
+test.describe('Test Suite 2', () => {
+    
+    test.beforeEach(async({page}) => {
+        await page.getByText('Tables & Data').click()
+    })
+
+    test('Navigate to Smart Table page', async({page}) => {
+        await page.getByText('Smart Table').click()
+    })
+
+    test('Navigate to Tree Greed page', async({page}) => {
+        await page.getByText('Tree Grid').click()
+    })
+
+})
+
