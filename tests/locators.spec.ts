49ee829f93e7ba5476658bbacad45afd1c16a34e
Bojan Peric <bperic.dev@gmail.com>
2024-07-21 10:17:25 +0200

    Create locators file with all PW locator types examples. Add data-testid to required HTML element. Rename original firstTest file.

---
+++ tests/locators.spec.ts
@@ -0,0 +1,56 @@
+import {test} from '@playwright/test'
+
+test.beforeEach(async({page}) => {
+    await page.goto('http://localhost:4200/')
+    await page.getByText('Forms').click()
+    await page.getByText('Form Layouts').click()
+})
+
+test('Locator syntax rules', async({page}) => {
+    
+    // by Tag name
+    page.locator('input')
+
+    // by Tag name (first in list)
+    page.locator('input').first()
+
+    // by ID
+    page.locator('#inputEmail1')
+
+    // by Class value (single value)
+    page.locator('.shape-rectangle')
+
+    // by Class value (full/all values)
+    page.locator('[class="input-full-width size-medium status-basic shape-rectangle nb-transition"]')
+
+    // by attribute
+    page.locator('[placeholder="Email"]')
+
+    // combine different selectors (ex. tag name + attribute + another attribute + class value single)
+    page.locator('input[placeholder="Email][nbinput].shape-rectangle"')
+
+    // by XPath (NOT RECOMMENDED)
+    page.locator('//*[@id="inputEmail1"]')
+
+    // by partial text match
+    page.locator(':text("Using")')
+
+    // by exact text match
+    page.locator(':text-is("Using the Grid")')
+
+})
+
+test('User facing locators', async({page}) => {
+    await page.getByRole('textbox', {name: "Email"}).first().click()
+    await page.getByRole('button', {name: "Sign in"}).first().click()
+
+    await page.getByLabel('Email').first().click()
+
+    await page.getByPlaceholder('Jane Doe').click()
+
+    await page.getByText('Using the Grid').click()
+
+    await page.getByTitle('IoT Dashboard').click()
+
+    await page.getByTestId('SignIn').click()
+})
